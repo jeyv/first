98326b823c136e6f2502ece59d8c62ddd0c1aaa7
Kevin Wilkes <kdw190001@utdallas.edu>
2021-12-02 15:28:47 -0600

    Changes to Dr.Jey's repo

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int counter=0;
 namespace two {
 	int counter=2;
 }
-
+//this is a change that was changed and such
 int main() {
 	cout << two::counter << endl;
 	cout << "It is nice to edit in Windows using VI" << endl;
-	cout <<  "This is Jey!" << endl;
+	cout <<  "This is Kevin	!" << endl;
 }
