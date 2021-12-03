29fa349294bc8ccac94ae29ddb9017f8ffedc816
Solomon Mathew <stm180001@utdallas.edu>
2021-12-03 17:32:16 -0600

    Changes to Dr.Jey's repo

--- main.cpp
+++ main.cpp
@@ -11,5 +11,4 @@ namespace two {
 int main() {
 	cout << two::counter << endl;
 	cout << "It is nice to edit in Windows using VI" << endl;
-	cout <<  "This is Thomas Kahng!" << endl;
-}
+	cout << "This is Solomon Mathew!" << endl;
